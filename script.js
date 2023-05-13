71b8a4d0db072770bcf98d4de7acde29bbf26df0
Kirill <91686805+kiryamakarov@users.noreply.github.com>
2023-05-13 16:27:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+function button_1() {
+    document.getElementById('panelPrompt').value='1';
+}
 
+function button_2() {
+    document.getElementById('panelPrompt').value+='2';
+}
+
+function button_3() {
+    document.getElementById('panelPrompt').value+='3';
+}
+
+function button_3() {
+    document.getElementById('panelPrompt').value+='3';
+}
+
+function button_4() {
+    document.getElementById('panelPrompt').value+='4';
+}
+
+function button_5() {
+    document.getElementById('panelPrompt').value+='5';
+}
+
+function button_6() {
+    document.getElementById('panelPrompt').value+='6';
+}
+
+function button_7() {
+    document.getElementById('panelPrompt').value+='7';
+}
+
+function button_8() {
+    document.getElementById('panelPrompt').value+='8';
+}
+
+function button_9() {
+    document.getElementById('panelPrompt').value+='9';
+}
+
+function button_0() {
+    document.getElementById('panelPrompt').value+='0';
+}
+
+function hard() {
+    document.getElementById("clickMe").onclick = doFunction;
+}
+
+function medium() {
+    document.getElementById("clickMe").onclick = doFunction;
+}
+
+function start() {
+    document.getElementById("clickMe").onclick = doFunction;
+}
